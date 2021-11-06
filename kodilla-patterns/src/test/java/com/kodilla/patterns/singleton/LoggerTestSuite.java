c209c072ec533ae6546ba2b8e83330a47ae4a1f5
Grzegorz T <gtadra@gmail.com>
2021-11-06 12:37:41 +0100

    Task 11.1 Logger singeton

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+import singleton.Logger;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoggerTestSuite {
+
+    @Test
+    void getLastLogTest() {
+        //Given
+        Logger logger = Logger.INSTANCE;
+        logger.log("new log");
+        //When
+        String lastLog = logger.getLastLog();
+        //Then
+        assertEquals("new log",lastLog);
+    }
+}
